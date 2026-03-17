2d040fed7d91cb09bfd0b890f5270490d7fc1942
Purnima Pandey <purnimapnd04@gmail.com>
2026-03-17 19:49:42 +0530

    Add missing coulumn to table- room

--- app/models/student.py
+++ app/models/student.py
@@ -6,6 +6,7 @@ class Student(Base):
     __tablename__= "students"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100))
+    room= Column(String(4), nullable=False)
     email = Column(String(100), unique=True, index=True, nullable=False)
     hashed_password = Column(String(200), nullable= False)
     status = Column(Enum(StudentStatusEnum), default=StudentStatusEnum.active, nullable= False)
