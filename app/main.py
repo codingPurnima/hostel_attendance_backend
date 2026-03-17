e8c785d467b0d4f59d4b5464748c624b377e08f9
Purnima Pandey <purnimapnd04@gmail.com>
2026-03-17 13:13:26 +0530

    Create student table

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-from app.routes import test_routes
+from app.routes import auth_routes
+from app.database import Base, engine
+from app.models import student
 
 app= FastAPI()
-app.include_router(test_routes.router, tags=["Test"])
+app.include_router(auth_routes.router, tags=["Authentication"])
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def home():
