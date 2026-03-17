bd0108e56d46ed6a3030612e3051ee4f3844ed1a
Purnima Pandey <purnimapnd04@gmail.com>
2026-03-17 13:04:53 +0530

    Create Student Status enum

---
+++ app/models/enums.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+# class RoleEnum(str, Enum):
+#     warden= "warden"
+#     resident= "resident"
+
+class StudentStatusEnum(str, Enum):
+    active="active"
+    onleave= "onLeave"
