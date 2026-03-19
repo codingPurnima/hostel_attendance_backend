50e22d463cda438f32e7a8876f8e91683884ca19
Purnima Pandey <purnimapnd04@gmail.com>
2026-03-19 21:14:35 +0530

    Verify face against stored embedding

--- app/routes/face_routes.py
+++ app/routes/face_routes.py
@@ -3,6 +3,8 @@ from typing import List
 from app.core.security import get_current_user
 from app.services.face_service import generate_embedding
 from app.database import SessionLocal, get_db
+import numpy as np
+import ast
 
 router= APIRouter()
 
@@ -42,10 +44,33 @@ async def register_face(
     db.commit()
     
     return{"message": "Face registered successfully"}
-
-
-
-
-
-
 # , description="Upload multiple images", media_type="multipart/form-data"
+
+# VERIFY USER IMAGE DURING ATTENDANCE
+@router.post("/verify")
+async def verify_face(
+    file: UploadFile= File(...),
+    current_user= Depends(get_current_user)
+):
+# GENERATE EMBEDDING FOR CAPTURED IMAGE 
+    image_bytes= await file.read()
+    new_embedding= generate_embedding(image_bytes)
+    if new_embedding is None:
+        return {"error": "Face not detected"}
+    
+# ACCESS FROM DATABASE 
+    stored_embedding= ast.literal_eval(current_user.face_embedding)
+
+    emb1= np.array(new_embedding)
+    emb2= np.array(stored_embedding)
+
+# COSINE SIMILARITY
+    similarity= np.dot(emb1, emb2) / (np.linalg.norm(emb1) * np.linalg.norm(emb2))
+
+    THRESHOLD= 0.6
+    is_match= bool(similarity> THRESHOLD)
+
+    return{
+        "match": bool(is_match),
+        "similarity": float(similarity)
+    }
