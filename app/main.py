cccc9eab7a6d649edb86264d4c748690f6658425
Purnima Pandey <purnimapnd04@gmail.com>
2026-03-27 12:54:21 +0530

    Request leaves and early returns

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routes import auth_routes,face_routes, test_routes, attendance_routes
+from app.routes import auth_routes,face_routes, test_routes, attendance_routes, leave_routes
 from app.database import Base, engine
 from app.models import student
 
@@ -8,6 +8,7 @@ app.include_router(auth_routes.router, prefix= "/auth", tags=["Authentication"])
 app.include_router(test_routes.router, tags=["Test"])
 app.include_router(face_routes.router, prefix="/face", tags=["Face"])
 app.include_router(attendance_routes.router, tags=["Attendance"])
+app.include_router(leave_routes.router, tags=["Leave"])
 
 
 Base.metadata.create_all(bind=engine) 
