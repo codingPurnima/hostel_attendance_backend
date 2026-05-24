4b0046f2f918498ed8e40a2e55c9cf1baaf80af6
Purnima Pandey <purnimapnd04@gmail.com>
2026-05-24 11:49:39 +0530

    Add leave reason and creation timestamp to table

--- app/models/leave_request.py
+++ app/models/leave_request.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, ForeignKey, Date, String
+from sqlalchemy import Column, Integer, ForeignKey, Date, String, DateTime, Enum
+from datetime import datetime, UTC
+from sqlalchemy.sql import func
 from app.database import Base
-from app.models.enums import AttendanceStatusEnum
+from app.models.enums import AttendanceStatusEnum, LeaveStatusEnum
 
 class LeaveRequest(Base):
     __tablename__ = "leave_requests"
@@ -9,4 +11,6 @@ class LeaveRequest(Base):
     student_id = Column(Integer, ForeignKey("students.id"))
     start_date = Column(Date)
     end_date = Column(Date)
-    status = Column(String(50), default=AttendanceStatusEnum.pending)
+    status = Column(Enum(LeaveStatusEnum), default=LeaveStatusEnum.pending)
+    reason= Column(String, nullable=False)
+    created_at= Column(DateTime(timezone=True), server_default=func.now())
